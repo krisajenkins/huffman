5ba64661755176f2372aa653fb8b27f2fa6fee43
Kris Jenkins <krisajenkins@gmail.com>
2014-07-17 20:25:34 +0100

    Removing the weight field from branch nodes.
    We can derive this information instead.

--- Main.hs
+++ Main.hs
@@ -6,39 +6,43 @@ import           Data.Map (Map)
 import qualified Data.Map as Map
 import           Data.Ord
 
-data Node = Leaf { symbol :: Char, weight :: Integer }
-          | Internal { left :: Node, right :: Node, weight :: Integer }
+data Node = Leaf { symbol :: Char, frequency :: Integer }
+          | Branch { left :: Node, right :: Node }
   deriving (Show,Read,Eq)
 
-frequency :: String -> Map Char Integer
-frequency = foldr f Map.empty
+weight :: Node -> Integer
+weight (Leaf {frequency = f}) = f
+weight (Branch {left = l, right = r}) = weight l + weight r
+
+charCount :: String -> Map Char Integer
+charCount = foldr f Map.empty
   where f :: Char -> Map Char Integer -> Map Char Integer
         f c = Map.insertWith (+) c 1
 
 freqToLeaves :: Map Char Integer -> [Node]
 freqToLeaves m = fmap f (Map.toList m)
-                   where f (k,v) = Leaf { symbol = k, weight = v}
+                   where f (k,v) = Leaf { symbol = k, frequency = v}
 
 firstPass :: String -> [Node]
-firstPass = freqToLeaves . frequency
+firstPass = freqToLeaves . charCount
 
 huffmanEncode :: [Node] -> Maybe Node
 huffmanEncode [] = Nothing
 huffmanEncode [n] = Just n
 huffmanEncode (n:m:ns) = huffmanEncode $ sortBy (comparing weight) (x:ns)
-                         where x = Internal {left = n, right = m, weight = total}
-                               total = (+) (weight n) (weight m)
+  where x = Branch {left = n, right = m}
 
-huffmanTable :: Num a => Maybe Node -> Map Char [a]
+huffmanTable :: Maybe Node -> Map Char [Integer]
 huffmanTable Nothing = Map.empty
 huffmanTable (Just (Leaf {symbol = s})) = Map.singleton s []
-huffmanTable (Just (Internal {left = l, right = r})) = Map.union leftMap rightMap
-                                                        where leftMap  = Map.map (0:) (huffmanTable (Just l))
-                                                              rightMap = Map.map (1:) (huffmanTable (Just r))
+huffmanTable (Just (Branch {left = l, right = r}))
+  = Map.union leftMap rightMap
+    where leftMap  = Map.map (0:) (huffmanTable (Just l))
+          rightMap = Map.map (1:) (huffmanTable (Just r))
 
 text :: String
 text = "The technique works by creating a binary tree of nodes. These can be stored in a regular array, the size of which depends on the number of symbols, n."
 
 main :: IO ()
-main = mapM_ (print . show) (Map.toList solution)
+main = mapM_ print (Map.toList solution)
  where solution = huffmanTable $ huffmanEncode $ firstPass text
